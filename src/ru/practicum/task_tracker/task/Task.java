4d0b147a44d35a4177c9f27b4bdde8817412b03b
soklen <soklenx@yandex.ru>
2024-05-24 03:34:50 +0300

    -- add class Task

---
+++ src/ru/practicum/task_tracker/task/Task.java
@@ -0,0 +1,69 @@
+package ru.practicum.task_tracker.task;
+
+import java.util.Objects;
+public class Task {
+    private Integer uniqueID;
+    private String name;
+    private String description;
+    private StateTask status;
+
+
+    public Task(String name, String description, StateTask status) {
+        this.name = name;
+        this.description = description;
+        this.status = status;
+    }
+
+    public Task(Integer uniqueID, String name, String description, StateTask status) {
+        this.uniqueID = uniqueID;
+        this.name = name;
+        this.description = description;
+        this.status = status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Integer getUniqueID() {
+        return uniqueID;
+    }
+
+    public void setUniqueID(Integer uniqueID) {
+        this.uniqueID = uniqueID;
+    }
+
+    public StateTask getStatus() {
+        return status;
+    }
+
+    public void setStatus(StateTask status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "uniqueID=" + uniqueID +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", status=" + status +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Task task)) return false;
+        return Objects.equals(getUniqueID(), task.getUniqueID()) && Objects.equals(getName(), task.getName()) && Objects.equals(getDescription(), task.getDescription()) && getStatus() == task.getStatus();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getUniqueID(), getName(), getDescription(), getStatus());
+    }
+}
