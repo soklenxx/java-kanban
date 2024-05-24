0f15655c2f2f9b922631658cf8cca97b58431e89
soklen <soklenx@yandex.ru>
2024-05-24 03:35:33 +0300

    -- add class Epic

---
+++ src/ru/practicum/task_tracker/task/Epic.java
@@ -0,0 +1,38 @@
+package ru.practicum.task_tracker.task;
+
+import java.util.ArrayList;
+
+public class Epic extends Task {
+    private ArrayList<Integer> subtasksIDs = new ArrayList<>();
+
+    public Epic(String name, String description, StateTask status) {
+        super(name, description, status);
+    }
+
+    public Epic(Integer uniqueID, String name, String description, StateTask status) {
+        super(uniqueID, name, description, status);
+    }
+
+    public ArrayList<Integer> getSubtasksID() {
+        return subtasksIDs;
+    }
+
+    public void addSubtaskIds(int subtaskID) {
+        subtasksIDs.add(subtaskID);
+    }
+
+    public void clearSubtaskIds () {
+        subtasksIDs.clear();
+    }
+
+    @Override
+    public String toString() {
+        return "Epic{" +
+                "uniqueID=" +  getUniqueID() +
+                ", name='" + getName() + '\'' +
+                ", description='" + getDescription() + '\'' +
+                ", status=" + getStatus() +
+                ", subtasksIDs=" + subtasksIDs +
+                '}';
+    }
+}
