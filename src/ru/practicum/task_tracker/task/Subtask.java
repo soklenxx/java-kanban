34372e70c4b7e5b5194eb6ef2024371b4e301a42
soklen <soklenx@yandex.ru>
2024-05-24 03:35:06 +0300

    -- add class Subtask

---
+++ src/ru/practicum/task_tracker/task/Subtask.java
@@ -0,0 +1,31 @@
+package ru.practicum.task_tracker.task;
+
+
+public class Subtask extends Task {
+    private Integer epicID;
+
+    public Subtask(String name, String description, StateTask status, Integer epicID) {
+        super(name, description, status);
+        this.epicID = epicID;
+    }
+
+    public Subtask(Integer uniqueID, String name, String description, StateTask status, Integer epicID) {
+        super(uniqueID, name, description, status);
+        this.epicID = epicID;
+    }
+
+    public Integer getEpicID() {
+        return epicID;
+    }
+
+    @Override
+    public String toString() {
+        return "Subtask{" +
+                "uniqueID=" +  getUniqueID() +
+                ", name='" + getName() + '\'' +
+                ", description='" + getDescription() + '\'' +
+                ", status=" + getStatus() +
+                ", epicID=" + epicID +
+                '}';
+    }
+}
