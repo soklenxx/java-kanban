f2833cf7606fe3a8a067aa02f2ce33fa4a822528
soklen <soklenx@yandex.ru>
2024-07-17 13:36:52 +0300

    -- fix checkStyle in Managers

--- src/ru/practicum/tracker/Managers.java
+++ src/ru/practicum/tracker/Managers.java
@@ -1,5 +1,4 @@
 package ru.practicum.tracker;
-
 import ru.practicum.tracker.manager.HistoryManager;
 import ru.practicum.tracker.manager.InMemoryHistoryManager;
 import ru.practicum.tracker.manager.InMemoryTaskManager;
@@ -7,9 +6,19 @@ import ru.practicum.tracker.manager.TaskManager;
 
 public class Managers {
 
-    private Managers() {}
-    public static TaskManager getDefault() {
-        return new InMemoryTaskManager();
+    private Managers() {
+
     }
-    public static HistoryManager getDefaultHistory() { return new InMemoryHistoryManager(); }
+
+    public static TaskManager getDefault() {
+
+        return new InMemoryTaskManager();
+
+    }
+
+    public static HistoryManager getDefaultHistory() {
+
+        return new InMemoryHistoryManager();
+
+    }
 }
