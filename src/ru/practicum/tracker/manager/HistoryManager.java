002a2683c274e5f707c52f640482e894eca22be4
soklen <soklenx@yandex.ru>
2024-07-17 13:09:49 +0300

    -- fix checkStyle in HistoryManager

--- src/ru/practicum/tracker/manager/HistoryManager.java
+++ src/ru/practicum/tracker/manager/HistoryManager.java
@@ -1,10 +1,9 @@
 package ru.practicum.tracker.manager;
-
 import ru.practicum.tracker.task.Task;
-
 import java.util.List;
 
 public interface HistoryManager {
+
     void add(Task task);
 
     List<Task> getHistory();
